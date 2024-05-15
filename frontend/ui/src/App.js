34467755b4214f18760e4e69a94bee61c0858ddb
Sujal7ss <sujalchahande3@gmail.com>
2024-05-16 01:35:53 +0530

    Initialized frontend file with React+Tailwind

---
+++ frontend/ui/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header font-bold text-4xl items-center flex flex-col" >
+        <h1>
+        Hello Contributors
+        </h1>
+        
+        
+        <h3>Break the previous UI into components   </h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
