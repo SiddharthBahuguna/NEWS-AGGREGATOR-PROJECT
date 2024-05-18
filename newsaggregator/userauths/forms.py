bca295f85460297d1fda85fb9481f3fecf05f7f0
rashiranjan22 <rashi.ranjan.cse22@itbhu.ac.in>
2024-05-18 17:25:15 +0530

    added password and username instruction, added error messages

--- newsaggregator/userauths/forms.py
+++ newsaggregator/userauths/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from userauths.models import User
+from django.core.exceptions import ValidationError
+
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -8,3 +10,9 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if len(username) < 6:
+            raise ValidationError("Username must be at least 6 characters long.")
+        return username
