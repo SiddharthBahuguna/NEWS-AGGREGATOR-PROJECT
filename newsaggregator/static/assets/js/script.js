07c2ba6114b15b5bd159e3cb250e8114affa8f04
anurag singh <anurag6569201@gmail.com>
2024-05-27 01:26:12 +0530

    fixed the bug of theme button fixes #75

--- newsaggregator/static/assets/js/script.js
+++ newsaggregator/static/assets/js/script.js
@@ -13,29 +13,3 @@ navMenuBtn.addEventListener('click', navToggleFunc);
 navCloseBtn.addEventListener('click', navToggleFunc);
 
 
-
-// theme toggle variables
-const themeBtn = document.querySelectorAll('.theme-btn');
-
-
-for (let i = 0; i < themeBtn.length; i++) {
-
-  themeBtn[i].addEventListener('click', function () {
-
-    // toggle `light-theme` & `dark-theme` class from `body`
-    // when clicked `theme-btn`
-    document.body.classList.toggle('light-theme');
-    document.body.classList.toggle('dark-theme');
-
-    for (let i = 0; i < themeBtn.length; i++) {
-
-      // When the `theme-btn` is clicked,
-      // it toggles classes between `light` & `dark` for all `theme-btn`.
-      themeBtn[i].classList.toggle('light');
-      themeBtn[i].classList.toggle('dark');
-
-    }
-
-  })
-
-}
