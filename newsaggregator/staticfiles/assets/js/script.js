d9b5a8490085d4399cacbd17544979c06a3cf127
Aryan <aryansharma220318@gmail.com>
2024-05-31 14:25:01 +0530

    instagram share functionality

---
+++ newsaggregator/staticfiles/assets/js/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// navbar variables
+const nav = document.querySelector('.mobile-nav');
+const navMenuBtn = document.querySelector('.nav-menu-btn');
+const navCloseBtn = document.querySelector('.nav-close-btn');
+
+
+// navToggle function
+const navToggleFunc = function () { nav.classList.toggle('active'); }
+
+navMenuBtn.addEventListener('click', navToggleFunc);
+navCloseBtn.addEventListener('click', navToggleFunc);
+
+
