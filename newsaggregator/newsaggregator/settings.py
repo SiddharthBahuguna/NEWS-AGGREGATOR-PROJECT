60951ba0d41af350912545442cee51daefc5db7a
napasaigopi-22 <cb.sc.p2cse24008@cb.students.amrita.edu>
2025-02-11 23:08:27 +0530

    firstpush

--- newsaggregator/newsaggregator/settings.py
+++ newsaggregator/newsaggregator/settings.py
@@ -16,7 +16,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
+# Quick-start development settings - unsuitable for productions
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
